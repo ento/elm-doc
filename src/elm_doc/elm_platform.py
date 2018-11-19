b36ce92de8477590dcf1f5a974bbed203dff60ec
Marica Odagaki <ento.entotto@gmail.com>
2018-11-18 16:07:35 -0800

    Add elm_platform.ELM_HOME

--- src/elm_doc/elm_platform.py
+++ src/elm_doc/elm_platform.py
@@ -1,9 +1,19 @@
+import os
 import subprocess
 from pathlib import Path
 import json
 
 from elm_doc import node_modules
 from elm_doc.decorators import capture_subprocess_error
+
+
+# See: https://github.com/elm/compiler/blob/0.19.0/builder/src/Elm/PerUserCache.hs#L44
+# Note: this implementation is not exactly the same as the above, which uses
+# getAppUserDataDirectory:
+# http://hackage.haskell.org/package/directory-1.3.3.1/docs/System-Directory.html#v:getAppUserDataDirectory
+# On Windows, this is something like 'C:/Users/<user>/AppData/Roaming/<app>'.
+# elm-doc currently don't support Windows, so this is fine for now.
+ELM_HOME = Path(os.environ['ELM_HOME']) if 'ELM_HOME' in os.environ else (Path.home() / '.elm')
 
 
 def install(to: Path, elm_version: str):
