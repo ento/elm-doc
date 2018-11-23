28b1646340032dc502b2974680ee0a738e097d3e
Marica Odagaki <ento.entotto@gmail.com>
2018-11-23 12:29:38 -0800

    Add dodo task to build elm.js

--- dodo.py
+++ dodo.py
@@ -2,6 +2,7 @@ import sys
 import os.path
 from tempfile import TemporaryDirectory
 import tarfile
+import json
 import subprocess
 import shutil
 from pathlib import Path
@@ -19,13 +20,13 @@ def task_create_elm_core_fixture():
         tarball = str(elm_core_fixture_path(elm_version))
         yield {
             'basename': 'create_elm_core_fixutre:' + elm_version,
-            'actions': [(create_elm_core_fixture, (elm_version, tarball))],
+            'actions': [(_create_elm_core_fixture, (elm_version, tarball))],
             'targets': [tarball],
             'uptodate': [True],
         }
 
 
-def create_elm_core_fixture(elm_version: str, tarball: str):
+def _create_elm_core_fixture(elm_version: str, tarball: str):
     os.makedirs(os.path.dirname(tarball), exist_ok=True)
     workspace_path = Path(__file__).parent / 'workspace'
     # clear elm-stuff because something in there causes CORRUPT BINARY
@@ -51,3 +52,65 @@ def create_elm_core_fixture(elm_version: str, tarball: str):
 
         with tarfile.open(tarball, "w:gz") as tar:
             tar.add(str(elm_home_path), arcname=elm_home_path.name)
+
+
+def task_create_package_elm_lang_org_artifact_tarball():
+    return {
+        'file_dep': [
+            'build/package.elm-lang.org/artifacts/elm.js',
+            'vendor/package.elm-lang.org/assets/favicon.ico',
+            'vendor/package.elm-lang.org/assets/highlight/highlight.pack.js',
+            'vendor/package.elm-lang.org/assets/highlight/LICENSE',
+            'vendor/package.elm-lang.org/assets/highlight/styles/default.css',
+            'vendor/package.elm-lang.org/assets/style.css',
+            'vendor/package.elm-lang.org/assets/help/documentation-format.md',
+            'vendor/package.elm-lang.org/assets/help/design-guidelines.md',
+        ],
+        'targets': [
+            'build/assets.tar.gz',
+        ],
+        'actions': [
+            'tar cf %(targets)s -C build/package.elm-lang.org/ artifacts',
+            'tar rf %(targets)s -C vendor/package.elm-lang.org/ assets',
+        ]
+    }
+
+
+def task_package_elm_lang_org_elm_js():
+    root_path = Path(__file__).parent.joinpath('vendor', 'package.elm-lang.org')
+    output_path = Path(__file__).parent.joinpath('build', 'package.elm-lang.org', 'artifacts', 'elm.js')
+    return {
+        'file_dep': list(root_path.glob('src/frontend/**/*.elm')) + [root_path / 'elm.json'],
+        'targets': [
+            'build/package.elm-lang.org/artifacts/elm.js',
+        ],
+        'actions': [(_create_package_elm_lang_org_elm_js, (output_path,))]
+    }
+
+
+def _create_package_elm_lang_org_elm_js(output_path: Path):
+    repo_path = Path(__file__).parent / 'vendor' / 'package.elm-lang.org'
+    with open(repo_path / 'elm.json') as f:
+        elm_json = json.load(f)
+    elm_version = elm_json['elm-version']
+    with TemporaryDirectory() as tmpdir:
+        root_path = Path(tmpdir)
+        elm_platform.install(root_path, elm_version)
+        elm_path = root_path / 'node_modules' / '.bin' / 'elm'
+        try:
+            subprocess.run(
+                [
+                    elm_path,
+                    'make',
+                    repo_path / 'src' / 'frontend' / 'Main.elm',
+                    '--output',
+                    str(output_path),
+                ],
+                cwd=repo_path,
+                check=True,
+                capture_output=True,
+            )
+        except subprocess.CalledProcessError as e:
+            print('\nSTDOUT:\n' + e.stdout.decode('utf8'))
+            print('\nSTDERR:\n' + e.stderr.decode('utf8'))
+            raise e
