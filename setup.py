d19272b0926f128d9c1b8418748c13885077a4ae
Marica Odagaki <ento.entotto@gmail.com>
2019-03-08 22:19:56 -0800

    Bump beta version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='elm-doc',
-    version='1.0.0b1',
+    version='1.0.0b2',
     url='https://github.com/ento/elm-doc',
     license='BSD 3-clause',
     author='Marica Odagaki',
