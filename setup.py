7f4adbf5ca12a6ed2177225e21e3418fceee0d1d
Marica Odagaki <ento.entotto@gmail.com>
2019-09-03 17:38:35 -0800

    Add 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Documentation',
     ]
