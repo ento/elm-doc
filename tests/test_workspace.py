46979bd4b61b28c087eeabd0769d5c65b7d7be35
ento <ento.entotto@gmail.com>
2020-05-19 10:02:47 -0800

    Support building fixtures/assets for 0.19.1

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -4,8 +4,8 @@ from elm_doc import elm_project
 from elm_doc import catalog_tasks
 
 
-def test_fixture_includes_popular_packages():
+def test_fixture_includes_popular_packages(elm_version):
     # this is so that tests can be run without network access, I think..
-    workspace_path = Path(__file__).parent.parent / 'workspace'
+    workspace_path = Path(__file__).parent.parent / 'workspace' / elm_version
     project = elm_project.from_path(workspace_path)
     assert set(project.direct_dependency_names()) >= set(catalog_tasks.popular_packages)
---
+++ workspace/0.19.1/src/Main.elm
@@ -0,0 +1,10 @@
+module Main exposing(main)
+
+{-| @docs main -}
+
+import Html exposing (Html, text)
+
+{-| main function -}
+main : Html ()
+main =
+  text "hello"
